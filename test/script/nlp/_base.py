e330c54032e67459f45021c0d0ff8c9c7ffdf91f
Mu Yang <emfomy@gmail.com>
2020-12-15 13:57:00 +0800

    Update unit test.

--- test/script/nlp/_base.py
+++ test/script/nlp/_base.py
@@ -8,23 +8,28 @@ __license__ = 'GPL-3.0'
 from ckip_transformers.nlp import *
 
 ################################################################################################################################
+# ckiplab/albert-tiny-chinese-#
 
 text = [
     '中文字耶，啊哈哈哈。',
     '「完蛋了！」 畢卡索他想',
+    '趙、錢、孫、李',
 ]
 
 ws = [
-    [ '中文字', '耶', '，', '啊', '哈', '哈哈', '。', ],
+    [ '中文字', '耶', '，', '啊哈', '哈哈', '。', ],
     [ '「', '完蛋', '了', '！', '」', ' ', '畢卡索', '他', '想', ],
+    [ '趙', '、', '錢', '、', '孫', '、', '李', ]
 ]
 
 pos = [
-    [ 'Na', 'T', 'COMMACATEGORY', 'I', 'D', 'D', 'PERIODCATEGORY', ],
+    [ 'Na', 'T', 'COMMACATEGORY', 'I', 'D', 'PERIODCATEGORY', ],
     [ 'PARENTHESISCATEGORY', 'VH', 'T', 'EXCLAMATIONCATEGORY', 'PARENTHESISCATEGORY', 'WHITESPACE', 'Nb', 'Nh', 'VE', ],
+    [ 'Nb', 'PAUSECATEGORY', 'Nb', 'PAUSECATEGORY', 'Nb', 'PAUSECATEGORY', 'Nb', ],
 ]
 
 ner = [
-    [ ( '中文字', 'LANGUAGE', (0, 3), ), ],
+    [],
     [ ( '畢卡索', 'PERSON', (7, 10), ), ],
+    [ ('趙', 'PERSON', (0, 1), ),  ('錢', 'PERSON', (2, 3), ),  ('孫', 'PERSON', (4, 5), ),  ('李', 'PERSON', (6, 7), ) ],
 ]
--- test/script/nlp/run.py
+++ test/script/nlp/run.py
@@ -10,21 +10,21 @@ from _base import *
 ################################################################################################################################
 
 def test_word_segmenter():
-    nlp = CkipWordSegmenter()
+    nlp = CkipWordSegmenter(level=1)
     output_ws = nlp(text)
     assert output_ws == ws
 
 ################################################################################################################################
 
 def test_pos_tagger():
-    nlp = CkipPosTagger()
+    nlp = CkipPosTagger(level=1)
     output_pos = nlp(ws)
     assert output_pos == pos
 
 ################################################################################################################################
 
 def test_ner_chunker():
-    nlp = CkipNerChunker()
+    nlp = CkipNerChunker(level=1)
     output_ner = nlp(text)
     output_ner = [ [ tuple(entity) for entity in sent ] for sent in output_ner ]
     assert output_ner == ner
