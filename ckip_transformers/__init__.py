64dd907b5f32e64c2f2349ede315b5ff3070bcca
Mu Yang <emfomy@gmail.com>
2022-06-26 16:49:06 +0800

    Bump to v0.3.0.

--- ckip_transformers/__init__.py
+++ ckip_transformers/__init__.py
@@ -10,7 +10,7 @@ __author_email__ = "emfomy@gmail.com"
 __copyright__ = "2021 CKIP Lab"
 
 __title__ = "CKIP Transformers"
-__version__ = "0.2.8"
+__version__ = "0.3.0"
 __description__ = "CKIP Transformers"
 __license__ = "GPL-3.0"
 
